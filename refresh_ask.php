109dd2b170fe08c1c08b45862b23dede03fc543a
Jonathan <109400928+JonathanCCY@users.noreply.github.com>
2023-07-23 22:07:00 +0800

    最新版本
    rem

--- refresh_ask.php
+++ refresh_ask.php
@@ -3,6 +3,7 @@
     require_once 'includes/dbh.inc.php';
     $sql_ask = "SELECT * FROM ask ORDER BY price ASC";
     $result_ask = mysqli_query($conn, $sql_ask);
+    $ask_volumes_sum = 0;
     if ($result_ask) {
         if (mysqli_num_rows($result_ask)>0) {
             while ($row = mysqli_fetch_assoc($result_ask)) {
@@ -13,8 +14,17 @@
     }
     for($i=4; $i>=0; $i--)
     {
+        $ask_volumes_sum += $ask_volumes[$i];
+    }
+    for($i=4; $i>=0; $i--)
+    {
+        $ask_vol_per = ($ask_volumes[$i] / $ask_volumes_sum) * 200;
+        $ask_vol_per = number_format($ask_vol_per, 2);
 ?>
     <ul><?php echo "$".$ask_prices[$i]; ?></ul>
+    <div class="progress-bar">
+        <div class="ask_bar" style="width: <?php echo $ask_vol_per; ?>%;"></div>
+    </div>
     <li><?php echo "Volume : ".$ask_volumes[$i]; ?></li>
 <?php
     }
--- refresh_bid.php
+++ refresh_bid.php
@@ -3,6 +3,7 @@
     require_once 'includes/dbh.inc.php';
     $sql_bid = "SELECT * FROM bid ORDER BY price DESC";
     $result_bid = mysqli_query($conn, $sql_bid);
+    $bid_volumes_sum = 0;
     if ($result_bid) {
         if (mysqli_num_rows($result_bid)>0) {
             while ($row = mysqli_fetch_assoc($result_bid)) {
@@ -13,8 +14,17 @@
     }
     for($i=0; $i<=4; $i++)
     {
+        $bid_volumes_sum += $bid_volumes[$i];
+    }
+    for($i=0; $i<=4; $i++)
+    {
+        $bid_vol_per = ($bid_volumes[$i] / $bid_volumes_sum) * 200;
+        $bid_vol_per = number_format($bid_vol_per, 2);
 ?>
     <ul><?php echo "$".$bid_prices[$i]; ?></ul>
+    <div class="progress-bar">
+        <div class="bid_bar" style="width: <?php echo $bid_vol_per; ?>%;"></div>
+    </div>
     <li><?php echo "Volume : ".$bid_volumes[$i]; ?></li>
 <?php
     }
