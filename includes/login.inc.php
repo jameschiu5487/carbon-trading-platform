cf6d0099af4a60f0cc0b78822850f782723b321b
Jonathan <109400928+JonathanCCY@users.noreply.github.com>
2023-07-27 22:02:29 +0800

    login page 改成sql語法

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,42 +1,91 @@
 <?php
+    // session_start();
+    // require_once 'dbh.inc.php';
+    // require_once 'functions.inc.php';
+    // if(isset($_POST["submit"])){
+    //     $username = $_POST["name"];
+    //     $pwd = $_POST["pwd"];
+    //     $_SESSION["useruid"] = $username;
+    //     if(isset($_SESSION["useruid"])){
+    //         $user = $_SESSION['useruid'];
+    //     }
+    //     $sql = "SELECT order_list.request, fill_list.time, order_list.id, fill_list.price, fill_list.volume
+    //             FROM fill_list
+    //             INNER JOIN order_list
+    //             ON order_list.user = '$user' AND (fill_list.id1 = order_list.id OR fill_list.id2 = order_list.id)
+    //             ORDER BY fill_list.filled_order_id DESC";
+    //     $result2 = mysqli_query($conn, $sql);
+    //     print_r($result2);
+    //     if (mysqli_num_rows($result2)>0) {
+    //         echo  "!!!";
+    //         while ($row = mysqli_fetch_assoc($result2)) {
+    //             $filled_request[] = $row['request'];
+    //         }
+    //         $count = count($filled_request);
+    //         $_SESSION["count"] = count($filled_request);
+    //     }
+    //     else {
+    //         $_SESSION["count"] = 0;
+    //     }            
+
+    //     if(emptyInputLogin($username, $pwd) !== false){
+    //         header("location: ../login.php?error=emptyinput");
+    //         exit();
+    //     }
+
+    //     loginUser($conn, $username, $pwd);
+    // }
+    // else{
+    //     header("location: ../login.php");
+    //     exit();
+    // }
+
+
     session_start();
     require_once 'dbh.inc.php';
     require_once 'functions.inc.php';
-    if(isset($_POST["submit"])){
+
+    if (isset($_POST["submit"])) {
         $username = $_POST["name"];
         $pwd = $_POST["pwd"];
         $_SESSION["useruid"] = $username;
-        if(isset($_SESSION["useruid"])){
+
+        if (isset($_SESSION["useruid"])) {
             $user = $_SESSION['useruid'];
         }
+
         $sql = "SELECT order_list.request, fill_list.time, order_list.id, fill_list.price, fill_list.volume
                 FROM fill_list
                 INNER JOIN order_list
-                ON order_list.user = '$user' AND (fill_list.id1 = order_list.id OR fill_list.id2 = order_list.id)
+                ON order_list.user = :user AND (fill_list.id1 = order_list.id OR fill_list.id2 = order_list.id)
                 ORDER BY fill_list.filled_order_id DESC";
-        $result2 = mysqli_query($conn, $sql);
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':user', $user);
+        $stmt->execute();
+        $result2 = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
         print_r($result2);
-        if (mysqli_num_rows($result2)>0) {
+
+        if (count($result2) > 0) {
             echo  "!!!";
-            while ($row = mysqli_fetch_assoc($result2)) {
+            foreach ($result2 as $row) {
                 $filled_request[] = $row['request'];
             }
             $count = count($filled_request);
             $_SESSION["count"] = count($filled_request);
-        }
-        else {
+        } else {
             $_SESSION["count"] = 0;
-        }            
+        }
 
-        if(emptyInputLogin($username, $pwd) !== false){
+        if (emptyInputLogin($username, $pwd) !== false) {
             header("location: ../login.php?error=emptyinput");
             exit();
         }
 
         loginUser($conn, $username, $pwd);
         // setcookie("user", $username, time() + 86400);        
-    }
-    else{
+    } else {
         header("location: ../login.php");
         exit();
     }
