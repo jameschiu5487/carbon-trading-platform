6e6acb4b3b690c52005059e01b90b9fcd9a64118
jameschiu5487 <jameschiu5487@gmail.com>
2023-07-27 10:44:43 +0800

    change config

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,12 +1,16 @@
 <?php
-
-$serverName = "carbon-trading.database.windows.net";
-$dBUsername = "IMFteam";
-$dBPassword = "NYCUimf5487";
-$dBName = "carbon-trading";
-
-$conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
-
-if(!$conn){
-    die("Connection Failed: " . mysqli_connect_error());
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:carbon-trading.database.windows.net,1433; Database = carbon-trading", "IMFteam", "NYCUimf5487");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "IMFteam", "pwd" => "NYCUimf5487", "Database" => "carbon-trading", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:carbon-trading.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
